c36b5b782d715014e8767a8b57dac96756f3093f
Jasmine Hegman <jasmine@webpt.com>
2014-10-27 18:23:46 -0700

    Replacing with a more durable autoload sequence

--- bin/jspackager.php
+++ bin/jspackager.php
@@ -1,6 +1,23 @@
 <?php
 
-require_once( dirname(__FILE__) . '/../vendor/autoload.php');
+
+$loaded = false;
+
+foreach (array(__DIR__ . '/../vendor/autoload.php', __DIR__ . '/vendor/autoload.php', __DIR__ . '/../../../autoload.php') as $file) {
+    if (file_exists($file)) {
+        require $file;
+        $loaded = true;
+        break;
+    }
+}
+
+if (!$loaded) {
+    die(
+        'You need to set up the project dependencies using the following commands:' . PHP_EOL .
+        'wget http://getcomposer.org/composer.phar' . PHP_EOL .
+        'php composer.phar install' . PHP_EOL
+    );
+}
 
 date_default_timezone_set('America/Denver');
 
